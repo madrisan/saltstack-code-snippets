899e11e3998c4acc75b1f055940d3afcf5777c50
Davide Madrisan <davide.madrisan@ext.soprasteria.com>
2017-02-17 19:49:46 +0200

    cpuinfo: use lscpu when available
    Signed-off-by: Davide Madrisan <davide.madrisan@ext.soprasteria.com>

--- cpuinfo.py
+++ cpuinfo.py
@@ -1,19 +1,21 @@
 # -*- coding: utf-8 -*-
 '''
-SaltStack code snippets.
-Module for managing system CPUs.
+SaltStack code snippets
+Modulr for managing system CPUs
+
 Copyright (C) 2017 Davide Madrisan <davide.madrisan.gmail.com>
+
 '''
+
 # Import 3rd-party libs
 import os
 
 # Import salt libs
 import salt.utils
+import salt.utils.fsutils
 from salt.exceptions import CommandExecutionError
 
-# Define the module's virtual name
 __virtualname__ = 'cpuinfo'
-
 proc_cpuinfo = '/proc/cpuinfo'
 
 def __virtual__():
@@ -21,18 +23,26 @@ def __virtual__():
         return (False, 'The {0} file cannot be found.'.format(proc_cpuinfo))
     return True
 
-def proc(*args):
+def _lscpu():
     '''
-    Return the number of core, logical, and CPU sockets, by parsing
-    the file /proc/cpuinfo.
-
-    CLI Example:
-
-        .. code-block:: bash
-
-            salt '*' cpuinfo.proc
-            salt '*' cpuinfo.proc logicals
+    Get available CPU information.
     '''
+    try: 
+        out = __salt__['cmd.run_all']("lscpu")
+    except:
+        return None
+    salt.utils.fsutils._verify_run(out)
+    data = dict()
+    for descr, value in [elm.split(":", 1) for elm in out['stdout'].split(os.linesep)]:
+        data[descr.strip()] = value.strip()
+
+    cpus = data.get('CPU(s)')
+    sockets = data.get('Socket(s)')
+    cores = data.get('Core(s) per socket')
+
+    return (cpus, sockets, cores)
+
+def _proc():
     cpus, cpu_core_id, cpu_physical_id = 0, set(), set()
 
     with salt.utils.fopen(proc_cpuinfo, 'r') as fp_:
@@ -46,8 +56,30 @@ def proc(*args):
         cores = len(cpu_core_id)
         sockets = len(cpu_physical_id)
 
+    if cores == 0 and sockets == 0:
+        # FIXME: assume that one core is installed
+        cores = 1
+        sockets = cpus
+
+    return (cpus, sockets, cores)
+
+def lscpu(*args):
+    '''
+    Return the number of core, logical, and CPU sockets, by parsing
+    the lscpu command and following back to /proc/cpuinfo when this tool
+    is not available.
+
+    CLI Example:
+
+        .. code-block:: bash
+
+            salt '*' cpuinfo.lscpu
+            salt '*' cpuinfo.lscpu logicals
+    '''
+    (cpus, sockets, cores) = _lscpu() or _proc()
+
     infos = {
-        'cores': cores, 
+        'cores': cores,
         'logicals': cpus,
         'sockets': sockets
     }
