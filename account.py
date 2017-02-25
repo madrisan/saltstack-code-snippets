1a91b900a1ff662ec3489725fe851a1abeabf36a
Davide Madrisan <davide.madrisan@ext.soprasteria.com>
2017-02-25 17:40:55 +0100

    account: use a more functional approach
    Signed-off-by: Davide Madrisan <davide.madrisan@ext.soprasteria.com>

--- account.py
+++ account.py
@@ -27,26 +27,23 @@ def get_group_list():
     file_group = '/etc/group'
     tokens = ('groupname', 'passwd', 'gid', 'grouplist')
     Group = namedtuple('Group', tokens)
-
-    def _secondary_groups(group):
-        grouplist = group.grouplist.strip()
-        return grouplist.split(',') if len(grouplist) > 0 else ''
-
     try:
         with salt.utils.fopen(file_group, 'r') as fp_:
-            group_infos = [Group(*line.split(':')) for line in fp_]
+            group_infos = list(Group(*line.split(':')) for line in fp_)
     except:
         raise CommandExecutionError(
             'An error has occurred while reading {0}'.format(file_group)
         )
 
-    return dict((
-        group.groupname, {
-            'gid': int(group.gid),
-            'grouplist': _secondary_groups(group)
-        }) if _secondary_groups(group) else (
-            group.groupname, {'gid': int(group.gid)})
-        for group in group_infos)
+    def _secondary_groups(group):
+        grouplist = group.grouplist.strip()
+        return grouplist.split(',') if len(grouplist) > 0 else ''
+    def _pack_data(group):
+        secgroups = _secondary_groups(group)
+        return dict(
+            gid = int(group.gid), grouplist = secgroups
+        ) if secgroups else dict(gid = int(group.gid))
+    return dict((group.groupname, _pack_data(group)) for group in group_infos)
 
 def get_user_list():
     '''
@@ -61,19 +58,19 @@ def get_user_list():
     file_user = '/etc/passwd'
     tokens = ('username', 'passwd', 'uid', 'gid', 'gecos', 'homedir', 'shell')
     User = namedtuple('User', tokens)
-
     try:
         with salt.utils.fopen(file_user, 'r') as fp_:
-            user_infos = [User(*line.rstrip().split(':')) for line in fp_]
+            user_infos = list(User(*line.rstrip().split(':')) for line in fp_)
     except:
         raise CommandExecutionError(
             'An error has occurred while reading {0}'.format(file_user)
         )
-    return dict((
-        user.username, {
-            'uid': int(user.uid),
-            'gid': int(user.gid),
-            'gecos': user.gecos,
-            'homedir': user.homedir,
-            'shell': user.shell
-        }) for user in user_infos)
+
+    def _pack_data(user):
+        return dict(
+            uid = int(user.uid),
+            gid = int(user.gid),
+            gecos = user.gecos,
+            homedir = user.homedir,
+            shell = user.shell)
+    return dict((user.username, _pack_data(user)) for user in user_infos)
